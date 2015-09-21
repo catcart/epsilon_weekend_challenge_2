7c26c78440a97df4463faa94a39c59fc7b27afca
Amelia <frevert.amelia@gmail.com>
2015-09-21 09:32:28 -0500

    Update wundergroundScripts.js

--- wundergroundScripts.js
+++ wundergroundScripts.js
@@ -4,13 +4,32 @@
   dataType : "jsonp",
     success : function(parsed_json) {
       console.log(parsed_json);
-    var location = parsed_json['location']['city'];
+      var location = parsed_json['location']['city'];
       var temp_f = parsed_json['current_observation']['temp_f'];
       var feelsLike = parsed_json['current_observation']['feelslike_f'];
-      $(".temperature").append("The temperature in Minneapolis today is " + temp_f + " degrees.");
-      $(".feelsLike").on("click", function(){
-        $(".randomInfo").append("Currently, it feels like " + feelsLike);
+      var uv = parsed_json['current_observation']['UV'];
+      var uvIndex = function()  {
+        parseInt(uv);
+      };
+      $(".temperature").append("The temperature in Minneapolis today is " + temp_f + "\xB0");
+      $(".feels").on("click", function()  {
+        $(".randomInfo").append("Currently, it feels like " + feelsLike + "\xB0");
+        $(this).remove();
       });
+    
+      function uvWarning(uv){
+        if(uv > 6) {
+          alert("Get out of the sun now!")
+        }
+          else if(uv == 5) {
+            alert("Wear sunscreen!")
+          }
+            else {
+              alert("No sunscreen...for now!")
+            }
+      }
+      uvWarning(uv);
+      console.log(uv);
     }
   });
 });
